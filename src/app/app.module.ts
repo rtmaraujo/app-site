a0b4ce45758bfbd2aa7c2b88cd312e9b73b4c8ec
Marcos Araujo <rtmaraujo@gmail.com>
2018-06-29 11:16:07 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultaComponent } from './pessoa/consulta/consulta.component';
+import { CadastroComponent } from './pessoa/cadastro/cadastro.component';
+
+import {routing} from './../app.routes';
+
+import { ConfigService } from './services/config.service';
+import { PessoaService } from './services/pessoa.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+	MenuComponent,
+	HomeComponent,
+	ConsultaComponent,
+	CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+	HttpModule,
+	FormsModule,
+	routing
+  ],
+  providers: [ConfigService,PessoaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
